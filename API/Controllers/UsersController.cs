4f4a4be780f6478a0dba75fd13251606d6fc9995
simerik1 <simerik16@gmail.com>
2024-06-23 15:38:05 +0530

    newApp

---
+++ API/Controllers/UsersController.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[Controller]")] ///api/Users
+    public class UsersController(DataContext context) : ControllerBase
+    {
+        
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers(){
+            var users = await context.Users.ToListAsync();
+            return users;
+            
+        }
+         [HttpGet("{Id:int}")]
+        public async Task<ActionResult<AppUser>> GetUser(int id){
+            var user= await context.Users.FindAsync(id);
+            if (user == null) return NotFound();
+            return user;
+            
+        }
+        
+    }
+}
