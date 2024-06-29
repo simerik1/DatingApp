5986a2e2ac72aaff37f79158876405ccb51f6071
simerik1 <simerik16@gmail.com>
2024-06-29 20:18:42 +0530

    creating register service

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -10,7 +10,7 @@ using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
 {
-   [Authorize]
+   
     public class UsersController(DataContext context) : BaseApiController
     {
         
