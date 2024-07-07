ddfca9dd22eb781870ee61e627599750e1b2052a
simerik1 <simerik16@gmail.com>
2024-07-07 10:40:53 +0530

    updating resources

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
 using API.Data;
 using API.Dtos;
@@ -13,25 +14,38 @@ using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
 {
-   [Authorize]
-    public class UsersController(IUserrRepository userrRepository) : BaseApiController
+    [Authorize]
+    public class UsersController(IUserrRepository userrRepository, IMapper mapper) : BaseApiController
     {
-        
-        
+
+
         [HttpGet]
-        public async Task<ActionResult<IEnumerable<MemberDto>>> GetUsers(){
+        public async Task<ActionResult<IEnumerable<MemberDto>>> GetUsers()
+        {
             var users = await userrRepository.GetMembersAsync();
 
             return Ok(users);
-            
+
         }
-         [HttpGet("{username}")]
-        public async Task<ActionResult<MemberDto>> GetUser(string username){
-            var user= await userrRepository.GetMemberAsync(username);
+        [HttpGet("{username}")]
+        public async Task<ActionResult<MemberDto>> GetUser(string username)
+        {
+            var user = await userrRepository.GetMemberAsync(username);
             if (user == null) return NotFound();
             return user;
-            
+
         }
-        
+        [HttpPut]
+        public async Task<ActionResult> UpdateUser(MemberUpdateDto memberUpdateDto)
+        {
+            var username = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+            if (username == null) return BadRequest("No username found in token");
+            var user = await userrRepository.GetUserByUsernameAsync(username);
+            if (user == null) return BadRequest("No user found ");
+            mapper.Map(memberUpdateDto, user);
+            if (await userrRepository.SaveAllAsync()) return NoContent();
+            return BadRequest("Failed to update the user");
+        }
+
     }
 }
--- client/src/app/app.config.ts
+++ client/src/app/app.config.ts
@@ -1,4 +1,4 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideAnimations } from '@angular/platform-browser/animations'
 
@@ -7,14 +7,17 @@ import { routes } from './app.routes';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { provideToastr } from 'ngx-toastr';
 import { jwtInterceptor } from './_interceptors/jwt.interceptor';
+import { NgxSpinnerModule } from 'ngx-spinner';
+import { loadingInterceptor } from './_interceptors/loading.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
   provideRouter(routes),
-  provideHttpClient(withInterceptors([jwtInterceptor])),
+  provideHttpClient(withInterceptors([jwtInterceptor, loadingInterceptor])),
   provideAnimations(),
   provideToastr({
     positionClass:'toast-bottom-right'
-  })
+  }),
+  importProvidersFrom(NgxSpinnerModule)
   ]
 };
